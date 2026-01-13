679ecb8f93775f88fa7e238c6cae81c0c0d451d2
trandat812003 <datnguyentran8@gamil.com>
2026-01-13 12:22:30 +0700

    add switchboard

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # stt
 
 ```bassh
-export HF_HUB_DISABLE_SYMLINKS=1 && hf download distil-whisper/earnings22 --repo-type dataset --local-dir /media/trandat/DataVoice/earnings22
+export HF_HUB_DISABLE_SYMLINKS=1 && hf download edinburghcstr/edacc --repo-type dataset --local-dir /media/trandat/DataVoice/edacc
+```
+
+```bash
+export HF_HUB_DISABLE_SYMLINKS=1 && hf download facebook/voxpopuli --repo-type dataset --local-dir /media/trandat/DataVoice/voxpopuli --include "data/en_accented/**"
+```
+
+```bash
+export HF_HUB_DISABLE_SYMLINKS=1 && hf download speechcolab/gigaspeech --repo-type dataset --local-dir /media/trandat/DataVoice/gigaspeech --exclude "data/audio/xl_files_additional/**"
+```
+
+```bash
+find . -type f -name "*.tar.gz" -exec tar -xzf {} \;
 ```
---
+++ src/switchboard/manifest_switchboard_dataset.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import json, os
+from pathlib import Path
+from tqdm import tqdm
+
+PARQUET_DIR = Path("/media/trandat/DataVoice/switchboard/data")
+OUT_DIR = Path("output")
+
+AUDIO_DIR = OUT_DIR / "audio"
+AUDIO_DIR.mkdir(parents=True, exist_ok=True)
+SPLITS = ["train", "validation", "test"]
+
+# tạo thư mục audio theo split
+for split in SPLITS:
+    (AUDIO_DIR / split).mkdir(exist_ok=True)
+
+manifests = {
+    "train": [],
+    "validation": [],
+    "test": []
+}
+
+global_idx = {
+    "train": 0,
+    "validation": 0,
+    "test": 0
+}
+
+# duyệt theo từng split
+for split in SPLITS:
+    parquet_files = sorted(PARQUET_DIR.glob(f"{split}-*.parquet"))
+    print(f"{split}: {len(parquet_files)} parquet files")
+
+    for parquet_file in parquet_files:
+        print(f"Processing {parquet_file.name}")
+        df = pd.read_parquet(parquet_file)
+
+        for _, row in tqdm(df.iterrows(), total=len(df)):
+            audio = row["audio"]
+
+            if isinstance(audio, dict):
+                # wav_bytes = audio["bytes"]
+                audio_name = audio["path"]
+            elif isinstance(audio, (bytes, bytearray)):
+                # wav_bytes = audio
+                audio_name = f"{split}_{global_idx[split]}.wav"
+            else:
+                raise TypeError(f"Unknown audio type: {type(audio)}")
+
+            # audio_path = AUDIO_DIR / split / audio_name
+
+            # with open(audio_path, "wb") as f:
+            #     f.write(wav_bytes)
+
+            manifests[split].append({
+                "file_id": os.path.splitext(audio_name)[0],
+                "file_path": f"{split} / {os.path.basename(audio_name)}",
+                "transcript": row["transcript"],
+                "sample_rate": row["sampling_rate"],
+            })
+
+            global_idx[split] += 1
+
+# lưu manifest theo split
+for split in SPLITS:
+    manifest_path = OUT_DIR / f"{split}_switchboard_manifest.jsonl"
+    with open(manifest_path, "w", encoding="utf-8") as f:
+        for r in manifests[split]:
+            f.write(json.dumps(r, ensure_ascii=False) + "\n")
+
+print("Done")
